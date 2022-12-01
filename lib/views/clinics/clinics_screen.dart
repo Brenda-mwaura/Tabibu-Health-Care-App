ca76fdbbbc9789db37db8c22c97041288210ae66
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-12-01 20:11:58 +0300

    :ok_hand: clinic screen

--- lib/views/clinics/clinics_screen.dart
+++ lib/views/clinics/clinics_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tabibu/configs/styles.dart';
 import 'package:tabibu/data/data_search.dart';
+import 'package:tabibu/views/home/components/home_list_view.dart';
 
 class ClinicScreen extends StatefulWidget {
   ClinicScreen({Key? key}) : super(key: key);
@@ -15,111 +16,138 @@ class _ClinicScreenState extends State<ClinicScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
-        drawer: const Drawer(),
-        body: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 125,
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.only(bottom: 20),
-                      width: MediaQuery.of(context).size.width,
-                      height: 85.0,
-                      decoration: const BoxDecoration(
-                        color: Styles.primaryColor,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(5.0),
-                          bottomRight: Radius.circular(5.0),
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Clinics",
-                          style: Styles.heading2(context),
-                        ),
+      key: _scaffoldKey,
+      drawer: const Drawer(),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 125,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    width: MediaQuery.of(context).size.width,
+                    height: 85.0,
+                    decoration: const BoxDecoration(
+                      color: Styles.primaryColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(5.0),
+                        bottomRight: Radius.circular(5.0),
                       ),
                     ),
-                    Positioned(
-                      top: 65.0,
-                      left: 0.0,
-                      right: 0.0,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              border: Border.all(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  width: 1.0),
-                              color: Colors.white),
-                          child: Row(
-                            children: [
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.menu,
-                                  color: Styles.primaryColor,
-                                ),
-                                onPressed: () {
-                                  print("your menu action here");
-                                  _scaffoldKey.currentState!.openDrawer();
-                                },
+                    child: Center(
+                      child: Text(
+                        "Clinics",
+                        style: Styles.heading2(context),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 65.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            border: Border.all(
+                                color: Colors.grey.withOpacity(0.5),
+                                width: 1.0),
+                            color: Colors.white),
+                        child: Row(
+                          children: [
+                            IconButton(
+                              icon: const Icon(
+                                Icons.menu,
+                                color: Styles.primaryColor,
                               ),
-                              Expanded(
-                                  child: InkWell(
+                              onPressed: () {
+                                print("your menu action here");
+                                _scaffoldKey.currentState!.openDrawer();
+                              },
+                            ),
+                            Expanded(
+                                child: InkWell(
+                              onTap: () {
+                                showSearch(
+                                    context: context, delegate: DataSearch());
+                              },
+                              child: TextField(
+                                autofocus: false,
                                 onTap: () {
                                   showSearch(
                                       context: context, delegate: DataSearch());
                                 },
-                                child: TextField(
-                                  autofocus: false,
-                                  onTap: () {
-                                    showSearch(
-                                        context: context,
-                                        delegate: DataSearch());
-                                  },
-                                  textInputAction: TextInputAction.search,
-                                  decoration: const InputDecoration(
-                                    hintText: "Search Clinic",
-                                    focusColor: Colors.white,
-                                    fillColor: Colors.white,
-                                  ),
+                                textInputAction: TextInputAction.search,
+                                decoration: const InputDecoration(
+                                  hintText: "Search Clinic",
+                                  focusColor: Colors.white,
+                                  fillColor: Colors.white,
                                 ),
-                              )),
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.search,
-                                  color: Styles.primaryColor,
-                                ),
-                                onPressed: () {
-                                  showSearch(
-                                      context: context, delegate: DataSearch());
-                                  print("your menu action here");
-                                },
                               ),
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.notifications,
-                                  color: Styles.primaryColor,
-                                ),
-                                onPressed: () {
-                                  print("your menu action here");
-                                },
+                            )),
+                            IconButton(
+                              icon: const Icon(
+                                Icons.search,
+                                color: Styles.primaryColor,
                               ),
-                            ],
-                          ),
+                              onPressed: () {
+                                showSearch(
+                                    context: context, delegate: DataSearch());
+                                print("your menu action here");
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(
+                                Icons.notifications,
+                                color: Styles.primaryColor,
+                              ),
+                              onPressed: () {
+                                print("your menu action here");
+                              },
+                            ),
+                          ],
                         ),
                       ),
-                    )
-                  ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            //
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(
+                  left: 15,
+                  right: 15,
                 ),
-              )
-            ],
-          ),
-        ));
+                child: Scrollable(
+                  viewportBuilder: (context, position) {
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: 20,
+                            itemBuilder: (context, index) {
+                              return HomePageListView();
+                            },
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
