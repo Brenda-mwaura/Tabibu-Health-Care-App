ecba62e31dbbe0e485359125773e9ea174281ba3
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-12-02 11:37:56 +0300

    :bug: fixes

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -22,179 +22,182 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       key: _scaffoldKey,
       drawer: const AppDrawer(),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 125,
-              child: Stack(
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    width: MediaQuery.of(context).size.width,
-                    height: 85.0,
-                    decoration: const BoxDecoration(
-                      color: Styles.primaryColor,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(5.0),
-                        bottomRight: Radius.circular(5.0),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 125,
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      width: MediaQuery.of(context).size.width,
+                      height: 85.0,
+                      decoration: const BoxDecoration(
+                        color: Styles.primaryColor,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(5.0),
+                          bottomRight: Radius.circular(5.0),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Dashboard",
+                          style: Styles.heading2(context),
+                        ),
                       ),
                     ),
-                    child: Center(
-                      child: Text(
-                        "Dashboard",
-                        style: Styles.heading2(context),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 65.0,
-                    left: 0.0,
-                    right: 0.0,
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8.0),
-                            border: Border.all(
-                                color: Colors.grey.withOpacity(0.5),
-                                width: 1.0),
-                            color: Colors.white),
-                        child: Row(
-                          children: [
-                            IconButton(
-                              icon: const Icon(
-                                Icons.menu,
-                                color: Styles.primaryColor,
+                    Positioned(
+                      top: 65.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              border: Border.all(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  width: 1.0),
+                              color: Colors.white),
+                          child: Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.menu,
+                                  color: Styles.primaryColor,
+                                ),
+                                onPressed: () {
+                                  print("your menu action here");
+                                  _scaffoldKey.currentState!.openDrawer();
+                                },
                               ),
-                              onPressed: () {
-                                print("your menu action here");
-                                _scaffoldKey.currentState!.openDrawer();
-                              },
-                            ),
-                            Expanded(
-                                child: InkWell(
-                              onTap: () {
-                                showSearch(
-                                    context: context, delegate: DataSearch());
-                              },
-                              child: TextField(
-                                autofocus: false,
+                              Expanded(
+                                  child: InkWell(
                                 onTap: () {
                                   showSearch(
                                       context: context, delegate: DataSearch());
                                 },
-                                textInputAction: TextInputAction.search,
-                                decoration: const InputDecoration(
-                                  hintText: "Search Clinic",
-                                  focusColor: Colors.white,
-                                  fillColor: Colors.white,
+                                child: TextField(
+                                  autofocus: false,
+                                  onTap: () {
+                                    showSearch(
+                                        context: context,
+                                        delegate: DataSearch());
+                                  },
+                                  textInputAction: TextInputAction.search,
+                                  decoration: const InputDecoration(
+                                    hintText: "Search Clinic",
+                                    focusColor: Colors.white,
+                                    fillColor: Colors.white,
+                                  ),
                                 ),
+                              )),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.search,
+                                  color: Styles.primaryColor,
+                                ),
+                                onPressed: () {
+                                  showSearch(
+                                      context: context, delegate: DataSearch());
+                                  print("your menu action here");
+                                },
                               ),
-                            )),
-                            IconButton(
-                              icon: const Icon(
-                                Icons.search,
-                                color: Styles.primaryColor,
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.notifications,
+                                  color: Styles.primaryColor,
+                                ),
+                                onPressed: () {
+                                  print("your menu action here");
+                                },
                               ),
-                              onPressed: () {
-                                showSearch(
-                                    context: context, delegate: DataSearch());
-                                print("your menu action here");
-                              },
-                            ),
-                            IconButton(
-                              icon: const Icon(
-                                Icons.notifications,
-                                color: Styles.primaryColor,
-                              ),
-                              onPressed: () {
-                                print("your menu action here");
-                              },
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              child: Container(
-                margin: const EdgeInsets.only(
-                  left: 15.0,
-                  right: 15.0,
+                    )
+                  ],
                 ),
-                child: Scrollable(
-                  viewportBuilder: (context, position) {
-                    return SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          const Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "Nearby Clinic",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 10),
-                          const SuggestedClinic(),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            children: [
-                              const Text(
-                                "Other Nearby facilities",
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                  ),
+                  child: Scrollable(
+                    viewportBuilder: (context, position) {
+                      return SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            const Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                "Nearby Clinic",
                                 style: TextStyle(
                                   color: Colors.black,
                                   fontSize: 20.0,
                                   fontWeight: FontWeight.w700,
                                 ),
                               ),
-                              const Spacer(),
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context)
-                                      .pushNamed(RouteGenerator.clinicPage);
-                                },
-                                child: const Text(
-                                  "See all",
+                            ),
+                            const SizedBox(height: 10),
+                            const SuggestedClinic(),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  "Other Nearby facilities",
                                   style: TextStyle(
-                                    color: Styles.primaryColor,
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w700,
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          ListView.builder(
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: 4,
-                            itemBuilder: (context, index) {
-                              return HomePageListView();
-                            },
-                          )
-                        ],
-                      ),
-                    );
-                  },
+                                const Spacer(),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.of(context)
+                                        .pushNamed(RouteGenerator.clinicPage);
+                                  },
+                                  child: const Text(
+                                    "See all",
+                                    style: TextStyle(
+                                      color: Styles.primaryColor,
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: 4,
+                              itemBuilder: (context, index) {
+                                return HomePageListView();
+                              },
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
