20b49abe125cf8f130303e0f8cd794beaab06f53
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-12-05 10:54:26 +0300

    :art: expandable text widget

--- lib/views/clinics/components/about_tabview.dart
+++ lib/views/clinics/components/about_tabview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tabibu/widgets/clinics_widget/expandable_description.dart';
 
 class AboutClinicTabView extends StatelessWidget {
   const AboutClinicTabView({Key? key}) : super(key: key);
@@ -57,10 +58,10 @@ class AboutClinicTabView extends StatelessWidget {
                   ],
                 ),
                 Container(
-                  height:double.infinity,
+                  width: MediaQuery.of(context).size.width,
+                  // height: MediaQuery.of(context).size.height,
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    // borderRadius: BorderRadius.only(),
                     boxShadow: [
                       BoxShadow(
                         color: Colors.grey.withOpacity(0.5),
@@ -70,12 +71,69 @@ class AboutClinicTabView extends StatelessWidget {
                       ),
                     ],
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      //clinic name and location below it
-                      Text(" ")
-                    ],
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 30,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // clinic name
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          'Afya Clinic Nairobi CBD',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        // location icon and then address
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            //location icon to be in a rounded grey container
+                            Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: const Icon(
+                                Icons.location_on,
+                                color: Colors.red,
+                              ),
+                            ),
+
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            const Text(
+                              'Makao Road, 67N',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                        // clinic description with a read more text that expands the description
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        ExpandableDescription(
+                          description:
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nunc, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc vel tincidunt lacinia, nunc nisl aliquam nunc, eget aliquam nisl nisl sit amet lorem.',
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
---
+++ lib/widgets/clinics_widget/expandable_description.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class ExpandableDescription extends StatefulWidget {
+  final String description;
+  ExpandableDescription({Key? key, required this.description})
+      : super(key: key);
+
+  @override
+  State<ExpandableDescription> createState() => _ExpandableDescriptionState();
+}
+
+class _ExpandableDescriptionState extends State<ExpandableDescription> {
+  late String firstHalf;
+  late String secondHalf;
+  bool flag = true;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.description.length > 30) {
+      firstHalf = widget.description.substring(0, 100);
+      secondHalf = widget.description.substring(100, widget.description.length);
+    } else {
+      firstHalf = widget.description;
+      secondHalf = "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: secondHalf.length == ""
+          ? Text(
+              widget.description,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  flag ? firstHalf + "..." : firstHalf + secondHalf,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      flag = !flag;
+                    });
+                  },
+                  child: Text(
+                    flag ? "Read more..." : "Read less...",
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}
