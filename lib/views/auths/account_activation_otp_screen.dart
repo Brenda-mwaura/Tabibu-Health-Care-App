7a1303f02bc5ad160f809d61a4ce903afbe7a422
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-12-21 20:02:18 +0300

    :construction_worker: activation otp feature

--- lib/views/auths/account_activation_otp_screen.dart
+++ lib/views/auths/account_activation_otp_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tabibu/configs/routes.dart';
+import 'package:tabibu/configs/styles.dart';
+import 'package:tabibu/providers/auth_provider.dart';
 import 'package:tabibu/views/auths/auth_base.dart';
 import 'package:tabibu/widgets/buttons/auth_button.dart';
 import 'package:tabibu/widgets/inputs/otp_input.dart';
+import 'package:tabibu/widgets/spinner.dart';
 
 class ActivationOTPScreen extends StatefulWidget {
   ActivationOTPScreen({Key? key}) : super(key: key);
@@ -19,6 +23,37 @@ class _ActivationOTPScreenState extends State<ActivationOTPScreen> {
   TextEditingController _pin4TextEditingController = TextEditingController();
   TextEditingController _pin5TextEditingController = TextEditingController();
   TextEditingController _pin6TextEditingController = TextEditingController();
+
+  Future activationOtpFnc() async {
+    if (otpFormKey.currentState!.validate()) {
+      var otp = _pin1TextEditingController.text +
+          _pin2TextEditingController.text +
+          _pin3TextEditingController.text +
+          _pin4TextEditingController.text +
+          _pin5TextEditingController.text +
+          _pin6TextEditingController.text;
+      if (otp.length == 6) {
+        await authProvider.otp(otp).then((value) {
+          if (value != null) {
+            Navigator.of(context).pushNamed(RouteGenerator.loginPage);
+            _pin1TextEditingController.text = "";
+            _pin2TextEditingController.text = "";
+            _pin3TextEditingController.text = "";
+            _pin4TextEditingController.text = "";
+            _pin5TextEditingController.text = "";
+            _pin6TextEditingController.text = "";
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text("Invalid OTP"),
+                backgroundColor: Styles.primaryColor,
+              ),
+            );
+          }
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -93,16 +128,21 @@ class _ActivationOTPScreenState extends State<ActivationOTPScreen> {
             ),
             const SizedBox(height: 30),
             AuthButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(RouteGenerator.loginPage);
-              },
-              child: const Text(
-                "Verify OTP",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w500,
-                ),
+              onPressed: activationOtpFnc,
+              child: Consumer<AuthProvider>(
+                builder: (context, value, child) {
+                  if (value.otpLoading == true) {
+                    return AppSpinner();
+                  }
+                  return const Text(
+                    "Verify OTP",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  );
+                },
               ),
             ),
           ],
