20cb2c8efb34135ddf712539b4b8a58065ff8541
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-12-28 18:18:58 +0300

    :ok_hand: clinics provider

---
+++ lib/providers/clinics_provider.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:tabibu/api/api.dart';
+import 'package:tabibu/providers/auth_provider.dart';
+
+class ClinicProvider extends ChangeNotifier {
+  bool _clinicsLoading = false;
+  bool get cinicsLoading => _clinicsLoading;
+
+  Future fetchClinics() async {
+    _clinicsLoading = true;
+    String? refreshToken = authProvider.allLoginDetails.refresh;
+
+    return Api.clinics().then((response) async {
+      var payload = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        notifyListeners();
+        _clinicsLoading = false;
+      } else if (response.statusCode == 401) {
+        await authProvider.refreshToken(refreshToken);
+        await fetchClinics();
+      } else {
+        _clinicsLoading = false;
+      }
+    }).catchError((error) {
+      _clinicsLoading = false;
+      print("error occured while fetching the clinics $error");
+    });
+  }
+}
+
+ClinicProvider clinicProvider = ClinicProvider();
