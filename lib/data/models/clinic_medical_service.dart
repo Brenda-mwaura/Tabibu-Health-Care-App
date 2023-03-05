b48aaf2254bebe160e47cd14d2434b6abd709aba
qinyanjuidavid <davidkinyanjui052@gmail.com>
2023-03-05 13:44:06 +0300

    :construction: medical service model

---
+++ lib/data/models/clinic_medical_service.dart
@@ -0,0 +1,46 @@
+
+import 'dart:convert';
+
+List<ClinicMedicalService> clinicMedicalServiceFromJson(String str) => List<ClinicMedicalService>.from(json.decode(str).map((x) => ClinicMedicalService.fromJson(x)));
+
+String clinicMedicalServiceToJson(List<ClinicMedicalService> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class ClinicMedicalService {
+    ClinicMedicalService({
+        this.id,
+        this.clinic,
+        this.service,
+        this.price,
+        this.available,
+        this.createdAt,
+        this.updatedAt,
+    });
+
+    int? id;
+    int? clinic;
+    int? service;
+    int? price;
+    bool? available;
+    DateTime? createdAt;
+    DateTime? updatedAt;
+
+    factory ClinicMedicalService.fromJson(Map<String, dynamic> json) => ClinicMedicalService(
+        id: json["id"],
+        clinic: json["clinic"],
+        service: json["service"],
+        price: json["price"],
+        available: json["available"],
+        createdAt: DateTime.parse(json["created_at"]),
+        updatedAt: DateTime.parse(json["updated_at"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "clinic": clinic,
+        "service": service,
+        "price": price,
+        "available": available,
+        "created_at": createdAt!.toIso8601String(),
+        "updated_at": updatedAt!.toIso8601String(),
+    };
+}
